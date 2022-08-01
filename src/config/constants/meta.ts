07df68342f3caa075494e09238ad6bd61b1f9c7b
ufogreys <45027262+ufogreys@users.noreply.github.com>
2022-08-01 04:23:01 -0300

    Update meta.ts

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -2,7 +2,7 @@ import { ContextApi } from 'contexts/Localization/types'
 import { PageMeta } from './types'
 
 export const DEFAULT_META: PageMeta = {
-  title: 'BriseSwap',
+  title: 'StoncksUSD',
   description:
     'The AMM you can trust on the BitGert network. Bringing you the true meaning of DeFi. Trade and farm tokens, passively, on our platform.',
   image: 'https://briseswap.finance/images/hero.png',
@@ -12,47 +12,47 @@ export const getCustomMeta = (path: string, t: ContextApi['t']): PageMeta => {
   switch (path) {
     case '/':
       return {
-        title: `${t('Home')} | ${t('BriseSwap')}`,
+        title: `${t('Home')} | ${t('StoncksUSD')}`,
       }
     case '/competition':
       return {
-        title: `${t('Trading Battle')} | ${t('BriseSwap')}`,
+        title: `${t('Trading Battle')} | ${t('StoncksUSD')}`,
       }
     case '/prediction':
       return {
-        title: `${t('Prediction')} | ${t('BriseSwap')}`,
+        title: `${t('Prediction')} | ${t('StoncksUSD')}`,
       }
     case '/farms':
       return {
-        title: `${t('Farms')} | ${t('BriseSwap')}`,
+        title: `${t('Farms')} | ${t('StoncksUSD')}`,
       }
     case '/pools':
       return {
-        title: `${t('Pools')} | ${t('BriseSwap')}`,
+        title: `${t('Pools')} | ${t('StoncksUSD')}`,
       }
     case '/lottery':
       return {
-        title: `${t('Lottery')} | ${t('BriseSwap')}`,
+        title: `${t('Lottery')} | ${t('StoncksUSD')}`,
       }
     case '/collectibles':
       return {
-        title: `${t('Collectibles')} | ${t('BriseSwap')}`,
+        title: `${t('Collectibles')} | ${t('StoncksUSD')}`,
       }
     case '/ifo':
       return {
-        title: `${t('Initial Farm Offering')} | ${t('BriseSwap')}`,
+        title: `${t('Initial Farm Offering')} | ${t('StoncksUSD')}`,
       }
     case '/teams':
       return {
-        title: `${t('Leaderboard')} | ${t('BriseSwap')}`,
+        title: `${t('Leaderboard')} | ${t('StoncksUSD')}`,
       }
     case '/profile/tasks':
       return {
-        title: `${t('Task Center')} | ${t('BriseSwap')}`,
+        title: `${t('Task Center')} | ${t('StoncksUSD')}`,
       }
     case '/profile':
       return {
-        title: `${t('Your Profile')} | ${t('BriseSwap')}`,
+        title: `${t('Your Profile')} | ${t('StoncksUSD')}`,
       }
     default:
       return null
