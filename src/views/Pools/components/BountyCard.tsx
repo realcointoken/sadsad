0c2340df2eff4a91f42cc73a23bfc89251b452eb
ufogreys <45027262+ufogreys@users.noreply.github.com>
2022-08-02 04:55:10 -0300

    Update BountyCard.tsx

--- src/views/Pools/components/BountyCard.tsx
+++ src/views/Pools/components/BountyCard.tsx
@@ -51,11 +51,11 @@ const BountyCard = () => {
       <Text mb="16px">{t('This bounty is given as a reward for providing a service to other users.')}</Text>
       <Text mb="16px">
         {t(
-          'Whenever you successfully claim the bounty, you’re also helping out by activating the Auto Bswap Pool’s compounding function for everyone.',
+          'Whenever you successfully claim the bounty, you’re also helping out by activating the Auto StoncksUSD Pool’s compounding function for everyone.',
         )}
       </Text>
       <Text style={{ fontWeight: 'bold' }}>
-        {t('Auto-Compound Bounty: %fee%% of all Auto Bswap pool users pending yield', { fee: callFee / 100 })}
+        {t('Auto-Compound Bounty: %fee%% of all Auto StoncksUSD pool users pending yield', { fee: callFee / 100 })}
       </Text>
     </>
   )
