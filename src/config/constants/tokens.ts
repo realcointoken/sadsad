16207c659f2376672dc82eb7a109ccc873cd5181
ufogreys <45027262+ufogreys@users.noreply.github.com>
2023-04-06 03:13:51 -0300

    Update tokens.ts

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -1,269 +1,28 @@
 const tokens = {
   bnb: {
-    symbol: 'BRISE',
-    projectLink: 'https://bitgert.com/',
+    symbol: 'BullChain',
+    projectLink: 'https://bullchainai.com/',
   },
   bswap: {
-    symbol: 'Bswap',
+    symbol: 'BC',
     address: {
-      32520: '0xF26006408112be347c23FDBa03F7bC3566519655',
+      911: '0x8db97c7cece249c2b98bdc0226cc4c2a57bf52fc',
       56: '',
       97: '',
     },
     decimals: 18,
-    projectLink: 'https://briseswap.finance/',
+    projectLink: 'https://bullchainai.com/',
   },
   bpad: {
-    symbol: 'BPAD',
+    symbol: 'IDXS',
     address: {
-      32520: '0x71946a5C9dA7C95ee804a9BE561EC15A3F286A7D',
+      911: '0x88Da55CE7185Fd86a2C15FE93b05F84E25cFeEB1',
       56: '',
       97: '',
     },
     decimals: 8,
     projectLink: 'https://brisepad.co/',
-  },
-  wbnb: {
-    symbol: 'WBRISE',
-    address: {
-      32520: '0x0eb9036cbE0f052386f36170c6b07eF0a0E3f710',
-      56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://bitgert.com/',
-  },
-  gert: {
-    symbol: 'GERT',
-    address: {
-      32520: '0x5057BBC3A847A79156a6cF628B75E6539adD87d2',
-      56: '',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://briseswap.finance/',
-  },
-  sphynx: {
-    symbol: 'SPHYNX',
-    address: {
-      32520: '0x0e11DCE06eF2FeD6f78CEF5144F970E1184b4298',
-      56: '',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://briseswap.finance/',
-  },
-  evo: {
-    symbol: 'EVO',
-    address: {
-      32520: '0x267Ae4bA9CE5ef3c87629812596b0D89EcBD81dD',
-      56: '',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://briseswap.finance/',
-  },
-  btxt: {
-    symbol: 'BTXT',
-    address: {
-      32520: '0x1A8a039007186d7640C1D7Cd7c2606e333D04e03',
-      56: '',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://briseswap.finance/',
-  },
-  omnia: {
-    symbol: 'OMNIA',
-    address: {
-      32520: '0x5d4685c2C75581C67b9D6292A065a767bC214681',
-      56: '',
-      97: '',
-    },
-    decimals: 8,
-    projectLink: 'https://briseswap.finance/',
-  },
-  vef: {
-    symbol: 'VEF',
-    address: {
-      32520: '0xD6447d2fA919811c41a064bDbdaB1E281F8de9B2',
-      56: '',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://briseswap.finance/',
-  },
-  prds: {
-    symbol: 'PRDS',
-    address: {
-      32520: '0x31226B28add9062c5064a9Bd35eA155F323C6ca6',
-      56: '',
-      97: '',
-    },
-    decimals: 9,
-    projectLink: 'https://briseswap.finance/',
-  },
-  brzilla: {
-    symbol: 'BRZILLA',
-    address: {
-      32520: '0x9b8535Dd9281e48484725bC9Eb6Ed2f66CEA2a36',
-      56: '',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://briseswap.finance/',
-  },
-  numi: {
-    symbol: 'NUMI',
-    address: {
-      32520: '0x6718e47e74497d1564EE76d832309144b83Ef8E8',
-      56: '',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://briseswap.finance/',
-  },
-  mapt: {
-    symbol: 'MAP',
-    address: {
-      32520: '0x6D347fdCb302a5879545E01EceE7A176db23dCDa',
-      56: '',
-      97: '',
-    },
-    decimals: 2,
-    projectLink: 'https://briseswap.finance/',
-  },
-  ypc: {
-    symbol: 'YPC',
-    address: {
-      32520: '0x11203a00a9134Db8586381C4B2fca0816476b3FD',
-      56: '',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://briseswap.finance/',
-  },
-  broge: {
-    symbol: 'BROGE',
-    address: {
-      32520: '0x41c5ae56681Fb19334eCF7d914919805DaE2Ec8f',
-      56: '',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://briseswap.finance/',
-  },
-  miidas: {
-    symbol: 'Miidas',
-    address: {
-      32520: '0x5B534A2Df329195Fd7e5c9AcA1D9ffbdA14A4963',
-      56: '',
-      97: '',
-    },
-    decimals: 6,
-    projectLink: 'https://briseswap.finance/',
-  },
-  yogo: {
-    symbol: 'YOGO',
-    address: {
-      32520: '0xB361D5953e21Cfde5CD62B89FDf40bc21903A6bb',
-      56: '',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://briseswap.finance/',
-  },
-  wmf: {
-    symbol: 'WMF',
-    address: {
-      32520: '0xc89fcd3E1CF5A355fc41E160d18BAC5f624610D4',
-      56: '',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://briseswap.finance/',
-  },
-  tokyo: {
-    symbol: 'TOKYO',
-    address: {
-      32520: '0x38EA4741d100cAe9700f66B194777F31919142Ee',
-      56: '',
-      97: '',
-    },
-    decimals: 9,
-    projectLink: 'https://tokyocoin.live/',
-  },
-  eltg: {
-    symbol: 'ELTG',
-    address: {
-      32520: '0xb860eCD8400600c13342a751408737235E177077',
-      56: '',
-      97: '',
-    },
-    decimals: 9,
-    projectLink: 'https://www.eltgraphen.com/',
-  },
-  mir: {
-    symbol: 'MIR',
-    address: {
-      32520: '0x2468dad471fA7E03d8029F057cc41742F017D53d',
-      56: '',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://mixedrealitygames.io/',
-  },
-  drv: {
-    symbol: 'DRV',
-    address: {
-      32520: '0xeB18A16A08530b811523fA49310319809ac4c979',
-      56: '',
-      97: '',
-    },
-    decimals: 9,
-    projectLink: 'https://darrival.com/',
-  },
-  baskom: {
-    symbol: 'BASKOM',
-    address: {
-      32520: '0x6cd08be8aa9b705ca86b4923b1784c0ee06e5220',
-      56: '',
-      97: '',
-    },
-    decimals: 9,
-    projectLink: 'https://brisecom.live/',
-  },
-  wolf: {
-    symbol: 'WOLF',
-    address: {
-      32520: '0x4Fb3DBF9111169ff60fFB8a7be1c6Fd3D4E417bC',
-      56: '',
-      97: '',
-    },
-    decimals: 19,
-    projectLink: 'https://alphatrades.co',
-  },
-  usdt: {
-    symbol: 'USDT',
-    address: {
-      32520: '0xDe14b85cf78F2ADd2E867FEE40575437D5f10c06',
-      56: '',
-      97: '',
-    },
-    decimals: 18,
-    projectLink: 'https://tether.to',
-  },
-  abr: {
-    symbol: 'ABR',
-    address: {
-      32520: '0x9F7Bb6E8386ac9ad5e944d66fBa80F3F7231FA94',
-      56: '',
-      97: '',
-    },
-    decimals: 9,
-    projectLink: 'https://aibra.io/',
   }
-  
 
   // qsd: {
   //   symbol: 'QSD',
